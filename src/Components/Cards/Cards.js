f5839724efaa54058c5dac8a2a4b56925d9fc970
Azhar Shah <azharshahsaintlouis@gmail.com>
2021-06-24 18:44:12 -0500

    More styling of Card Component

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -12,14 +12,14 @@ const Cards = ({ gitInfo }) => {
         display = gitInfo.map(git => {
             return (<div className={classes.CardsContainer}>
                 <div className={classes.LeftSide}>
-                    <div>{git.description}</div>
-                    <div>{git.Author}</div>
-                    <div>{git.Title}</div>
+                    <div className={classes.Description}>{git.description === null ? <p>No Description </p> : <p>{git.description}</p>}</div>
+                    <div className={classes.Author}>{git.Author}</div>
+                    <div className={classes.Title}>{git.Title}</div>
                 </div>
                 <div className={classes.RightSide}>
-                    <div>{git.forks}</div>
-                    <div>{git.stars}</div>
-                    <div>{git.open_issues}</div>
+                    <div className={classes.Forks}>{git.forks} forks</div>
+                    <div className={classes.Stars}>{git.stars} stars</div>
+                    <div className={classes.Issues}>{git.open_issues} open issues</div>
                 </div>
             </div>
             )
