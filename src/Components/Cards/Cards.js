9cd53be002119b2954886ba82d88d5679041d8e5
Azhar Shah <azharshahsaintlouis@gmail.com>
2021-06-25 07:36:12 -0500

    Finished styling card

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -3,10 +3,10 @@ import classes from './Cards.module.css'
 import Spinner from '../Spinner/Spinner'
 import Star from '../../assets/images/star.png'
 import Fork from '../../assets/images/code-fork-symbol.png'
+import Circle from '../../assets/images/circle.png'
 
 const Cards = ({ gitInfo }) => {
 
-    console.log(gitInfo)
     let display;
     if (gitInfo === undefined) {
         display = <Spinner />
@@ -21,7 +21,7 @@ const Cards = ({ gitInfo }) => {
                 <div className={classes.RightSide}>
                     <div className={classes.Forks}><img src={Fork} alt="coding fork" /> {git.forks} forks</div>
                     <div className={classes.Stars}><span><img src={Star} alt="star" /></span><span>{git.stars} stars</span></div>
-                    <div className={classes.Issues}>{git.open_issues} open issues</div>
+                    <div className={classes.Issues}><span><img src={Circle} alt="circle" /></span>{git.open_issues} open issues</div>
                 </div>
             </div>
             )
